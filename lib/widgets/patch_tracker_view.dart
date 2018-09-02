55edd666746468182970dd250962c03121416c08
Franklin Wang <fractalemagic@gmail.com>
2018-09-02 15:50:56 +1200

    Auto update progress bar & show day of week if necessary

--- lib/widgets/patch_tracker_view.dart
+++ lib/widgets/patch_tracker_view.dart
@@ -1,10 +1,13 @@
+import 'dart:async';
+
 import 'package:app/farming/growth_calculator.dart';
+import 'package:app/inject/inject.dart';
 import 'package:app/inject/injector.dart';
 import 'package:app/models/farming_patch.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class PatchTrackerView extends StatelessWidget {
+class PatchTrackerView extends StatefulWidget {
   const PatchTrackerView({
     Key key,
     @required this.patch,
@@ -14,15 +17,58 @@ class PatchTrackerView extends StatelessWidget {
   final FarmingPatch patch;
 
   @override
-  Widget build(BuildContext context) {
-    final growthCalculator = Injector.of(context).get<GrowthCalculator>();
+  PatchTrackerViewState createState() => new PatchTrackerViewState();
+}
 
+class PatchTrackerViewState extends State<PatchTrackerView> {
+  Inject _inject;
+
+  GrowthCalculator _growthCalculator;
+
+  Future<Null> _tick() async {
+    if (!mounted) {
+      return;
+    }
+
+    // Invalidate the view to redraw the progress bar
+    setState(() {});
+
+    // Schedule the next tick
+    final tickLength = Duration(seconds: 1);
+    await Future.delayed(tickLength, _tick);
+  }
+
+  void _init() {
+    _growthCalculator = _inject.get();
+
+    _tick();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_inject == null) {
+      _inject = Injector.of(context);
+      _init();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final now = DateTime.now();
-    final finishAt = growthCalculator.estimate(patch);
+    final patch = widget.patch;
+
+    // Calculate progress bar value
+    final finishAt = _growthCalculator.estimate(patch);
     final duration = finishAt.difference(patch.plantedAt);
     final elapsed = now.difference(patch.plantedAt);
     final progress = elapsed.inSeconds / duration.inSeconds;
-    final finishAtText = DateFormat('h:mm a').format(finishAt);
+
+    // Generate finish time to display - include day of week if necessary
+    final sameDay = finishAt.day == patch.plantedAt.day;
+    final dateFormat = sameDay ? 'h:mm a' : 'EEEE h:mm a';
+    final finishAtText = DateFormat(dateFormat).format(finishAt);
 
     return Card(
       child: Stack(
